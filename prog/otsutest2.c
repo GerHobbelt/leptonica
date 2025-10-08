556d33de88fecdc7fa99e67a1d0077562a9218cb
Ger Hobbelt <ger@hobbelt.com>
2025-10-08 15:51:13 +0200

    augmented otsutest2 to run multiple grid sizes for the Otsu setting: previously pixOtsuAdaptiveThreshold() was used in a manner that enforced image-as-single-cell treatment for any but the largest (> 2000x2000px) images, which is simple perhaps, but does not showcase the 'adaptive' behaviour, nor the smoothing parameter settings. Now we run a series of Otsu operations, with varying parameter settings, on each image: f.e., the image grid runs from the original 1x1 down to the tiniest allowed, where each cell is 16px x 16px: the minimum supported by the leptonica adaptive Otsu implementation.

--- prog/otsutest2.c
+++ prog/otsutest2.c
@@ -60,17 +60,17 @@
 #endif
 
 int main(int    argc,
-         const char **argv)
+	const char** argv)
 {
-char       textstr[L_MAX(256, MAX_PATH)];
-l_int32    i, thresh, fgval, bgval;
-l_float32  scorefract;
-L_BMF     *bmf;
-PIX       *pixs, *pixb, *pixg, *pixp, *pix1, *pix2, *pix3, *pixth;
-PIXA      *pixa1, *pixad;
+	char       textstr[L_MAX(256, MAX_PATH)];
+	l_int32    i, thresh, fgval, bgval;
+	l_float32  scorefract;
+	L_BMF* bmf;
+	PIX* pixs, * pixb, * pixg, * pixp, * pix1, * pix2, * pix3, * pixth;
+	PIXA* pixa1, * pixad;
 
-    setLeptDebugOK(1);
-    lept_mkdir("lept/otsu");
+	setLeptDebugOK(1);
+	lept_mkdir("lept/otsu");
 
 	SARRAY* sargv = NULL;
 	if (argc <= 1)
@@ -85,106 +85,155 @@ PIXA      *pixa1, *pixad;
 	}
 
 	int argv_count = sarrayGetCount(sargv);
-  for (int argidx = 0; argidx < argv_count; argidx++)
-  {
-	  const char* filename = sarrayGetString(sargv, argidx, L_NOCOPY);
-	  const char* filepath = DEMOPATH(filename);
-	  filename = getPathBasename(filepath, FALSE);
-	  char* sani_filename = sanitizePathToIdentifier(NULL, 70, argidx + 1, filepath, "@#_-");
+	for (int argidx = 0; argidx < argv_count; argidx++)
+	{
+		const char* filename = sarrayGetString(sargv, argidx, L_NOCOPY);
+		const char* filepath = DEMOPATH(filename);
+		filename = getPathBasename(filepath, FALSE);
+		char* sani_filename = sanitizePathToIdentifier(NULL, 70, argidx + 1, filepath, "@#_-");
 
 
-	  lept_stderr("\n\n\nProcessing image #%d: %s = %s :: %s\n", argidx + 1, filename, filepath, sani_filename);
+		lept_stderr("\n\n\nProcessing image #%d: %s = %s :: %s\n", argidx + 1, filename, filepath, sani_filename);
 
 
-    pixs = pixRead(filepath);
-    pixg = pixConvertTo8(pixs, 0);
-    bmf = bmfCreate(NULL, 8);
-    pixad = pixaCreate(0);
-    for (i = 0; i < 3; i++) {
-        pixa1 = pixaCreate(2);
-        scorefract = 0.1 * i;
+		pixs = pixRead(filepath);
+		pixg = pixConvertTo8(pixs, 0);
 
-		/* Show the histogram of gray values and the split location */
-		pixSplitDistributionFgBg(pixg, scorefract, 1,
-			&thresh, &fgval, &bgval, &pixp);
-		lept_stderr("thresh = %d, fgval = %d, bgval = %d\n",
-			thresh, fgval, bgval);
-		pixaAddPix(pixa1, pixg, L_COPY);
-		pixaAddPix(pixa1, pixp, L_INSERT);
-		// add 1 empty slot to fill the pixa row:
-		{
-			PIX* dummy = pixCreate(1, 1, 1);
-			pixaAddPix(pixa1, dummy, L_INSERT);
+		int prev_sx = 0, prev_sy = 0;
+		int w, h;
+		pixGetDimensions(pixs, &w, &h, NULL);
+
+		bmf = bmfCreate(NULL, 8);
+		pixad = pixaCreate(0);
+		for (int grid = 1; grid <= L_MIN(w / 2, h / 2); grid += L_MAX(1, grid / 2)) {
+			// aim for single-tile processing:
+			int sx = w / grid;
+			int sy = h / grid;
+			// and just to show a clear single-grid-cell sx value: 2000, for the first round.
+			if (grid == 1) {
+				sx = L_MAX(2000, sx);
+				sy = L_MAX(2000, sy);
+			}
+
+			// apply sanity limits that are required by the Otsu APIs: if we don't do this, those will error out anyway!
+			sx = L_MAX(16, sx);
+			sy = L_MAX(16, sy);
+
+			if (sx == prev_sx && sy == prev_sy) {
+				// force loop termination!
+				break;
+			}
+
+			for (i = 0; i < 3; i++) {
+				pixa1 = pixaCreate(2);
+				scorefract = 0.1 * i;
+				lept_stderr("\nScorefrac: %1.3f, Grid: %d\n", scorefract, grid);
+
+				/* Show the histogram of gray values and the split location */
+				pixSplitDistributionFgBg(pixg, scorefract, 1,
+					&thresh, &fgval, &bgval, &pixp);
+				lept_stderr("thresh = %d, fgval = %d, bgval = %d\n",
+					thresh, fgval, bgval);
+				pixaAddPix(pixa1, pixs, L_COPY);
+				pixaAddPix(pixa1, pixg, L_COPY);
+				pixaAddPix(pixa1, pixp, L_INSERT);
+
+				for (int j = 0; j <= 3; j++) {
+
+					/* Get a 1 bpp version; use a single tile */
+					pixOtsuAdaptiveThreshold(pixg, sx, sy, j, j, scorefract,
+						&pixth, &pixb);
+					pixaAddPix(pixa1, pixg, L_COPY);
+					pixaAddPix(pixa1, pixb, L_INSERT);
+
+					// sampled-scaling of the threshold 'image' to the same size as the source image, so one can properly inspect the cell threshold colors:
+					l_int32    thw, thh;
+					l_float32  scalex, scaley;
+
+					pixGetDimensions(pixth, &thw, &thh, NULL);
+					scalex = (l_float32)w / (l_float32)thw;
+					scaley = (l_float32)h / (l_float32)thh;
+
+					pix2 = pixScaleBySamplingWithShift(pixth, scalex, scaley, 0, 0);
+					pixDestroy(&pixth);
+
+					pixaAddPix(pixa1, pix2, L_INSERT);
+
+					/* improved version of the API: */
+
+					/* Get a 1 bpp version; use a single tile */
+					pixOtsuAdaptiveThreshold2(pixg, sx, sy, j, j, scorefract,
+						&pixth, &pixb);
+					pixaAddPix(pixa1, pixg, L_COPY);
+					pixaAddPix(pixa1, pixb, L_INSERT);
+
+					// sampled-scaling of the threshold 'image' to the same size as the source image, so one can properly inspect the cell threshold colors:
+					pixGetDimensions(pixth, &thw, &thh, NULL);
+					scalex = (l_float32)w / (l_float32)thw;
+					scaley = (l_float32)h / (l_float32)thh;
+
+					pix2 = pixScaleBySamplingWithShift(pixth, scalex, scaley, 0, 0);
+					pixDestroy(&pixth);
+
+					pixaAddPix(pixa1, pix2, L_INSERT);
+				}
+
+				/* Join these together and add some text */
+				pix1 = pixaDisplayTiledInColumns(pixa1, 3, 1.0, 20, 2);
+				snprintf(textstr, sizeof(textstr),
+					"Scorefract = %3.1f\nGrid: %d x %d (cell #: %d x %d)\nH x W: %d x %d\nThresh = %d (%s)", scorefract, sx, sy, grid, grid, pixGetHeight(pixg), pixGetWidth(pixg), thresh, filename);
+				pix2 = pixAddSingleTextblock(pix1, bmf, textstr, 0x06670f00,
+					L_ADD_BELOW, NULL);
+
+				/* Save and display the result */
+				pixaAddPix(pixad, pix2, L_INSERT);
+				snprintf(textstr, sizeof(textstr), "/tmp/lept/otsu/%s.%03d.Grid-%02d.ScoreFrac-%03d.png", sani_filename, argidx + 1, grid, (int)i);
+				pixWrite(textstr, pix2, IFF_PNG);
+				pixDisplayWithTitle(pix2, 100, 100, filepath, TRUE);
+				pixDestroy(&pix1);
+				pixaDestroy(&pixa1);
+			}
+
+			prev_sx = sx;
+			prev_sy = sy;
 		}
 
-		for (int j = 0; j <= 3; j++) {
-
-			/* Get a 1 bpp version; use a single tile */
-			pixOtsuAdaptiveThreshold(pixg, 2000, 2000, j, j, scorefract,
-				&pixth, &pixb);
-			pixaAddPix(pixa1, pixg, L_COPY);
-			pixaAddPix(pixa1, pixb, L_INSERT);
-			pixaAddPix(pixa1, pixth, L_INSERT);
-
-			/* improved version of the API: */
-
-			/* Get a 1 bpp version; use a single tile */
-			pixOtsuAdaptiveThreshold2(pixg, 2000, 2000, j, j, scorefract,
-				&pixth, &pixb);
-			pixaAddPix(pixa1, pixg, L_COPY);
-			pixaAddPix(pixa1, pixb, L_INSERT);
-			pixaAddPix(pixa1, pixth, L_INSERT);
+		/* Use a smaller tile for Otsu */
+		for (i = 0; i < 2; i++) {
+			scorefract = 0.1 * i;
+			pixOtsuAdaptiveThreshold(pixg, 300, 300, 0, 0, scorefract,
+				NULL, &pixb);
+			pix1 = pixAddBlackOrWhiteBorder(pixb, 2, 2, 2, 2, L_GET_BLACK_VAL);
+			//pix2 = pixScale(pix1, 0.5, 0.5);
+			pix2 = pixClone(pix1);
+			snprintf(textstr, sizeof(textstr),
+				"Scorefract = %3.1f (%s)", scorefract, filename);
+			pix3 = pixAddSingleTextblock(pix2, bmf, textstr, 1,
+				L_ADD_BELOW, NULL);
+			snprintf(textstr, sizeof(textstr), "/tmp/lept/otsu/%s.%03d.%03d.300.png", sani_filename, argidx + 1, (int)i);
+			pixWrite(textstr, pix3, IFF_PNG);
+			pixaAddPix(pixad, pix3, L_INSERT);
+			pixDestroy(&pixb);
+			pixDestroy(&pix1);
+			pixDestroy(&pix2);
 		}
 
-		/* Join these together and add some text */
-        pix1 = pixaDisplayTiledInColumns(pixa1, 3, 1.0, 20, 2);
-        snprintf(textstr, sizeof(textstr),
-             "Scorefract = %3.1f ........... Thresh = %d (%s)", scorefract, thresh, filename);
-        pix2 = pixAddSingleTextblock(pix1, bmf, textstr, 0x00ff0000,
-                                     L_ADD_BELOW, NULL);
+		snprintf(textstr, sizeof(textstr), "/tmp/lept/otsu/%s.result.pdf", sani_filename);
+		char* out_fullname = genPathname(textstr, NULL);
+		lept_stderr("Writing to: %s --> %s\n", textstr, out_fullname);
+		pixaConvertToPdf(pixad, 75, 1.0, 0, 0, "Otsu thresholding", out_fullname);
+		stringDestroy(&out_fullname);
+		bmfDestroy(&bmf);
+		pixDestroy(&pixs);
+		pixDestroy(&pixg);
+		pixaDestroy(&pixad);
 
-            /* Save and display the result */
-        pixaAddPix(pixad, pix2, L_INSERT);
-        snprintf(textstr, sizeof(textstr), "/tmp/lept/otsu/%03d.%03d.%s.png", argidx + 1, (int)i, sani_filename);
-        pixWrite(textstr, pix2, IFF_PNG);
-		pixDisplayWithTitle(pix2, 100, 100, filepath, TRUE);
-        pixDestroy(&pix1);
-        pixaDestroy(&pixa1);
-    }
+		LEPT_FREE(filename);
+		LEPT_FREE(sani_filename);
+		LEPT_FREE(filepath);
+	}
 
-        /* Use a smaller tile for Otsu */
-    for (i = 0; i < 2; i++) {
-        scorefract = 0.1 * i;
-        pixOtsuAdaptiveThreshold(pixg, 300, 300, 0, 0, scorefract,
-                                 NULL, &pixb);
-        pix1 = pixAddBlackOrWhiteBorder(pixb, 2, 2, 2, 2, L_GET_BLACK_VAL);
-        pix2 = pixScale(pix1, 0.5, 0.5);
-        snprintf(textstr, sizeof(textstr),
-             "Scorefract = %3.1f (%s)", scorefract, filename);
-        pix3 = pixAddSingleTextblock(pix2, bmf, textstr, 1,
-                                     L_ADD_BELOW, NULL);
-        pixaAddPix(pixad, pix3, L_INSERT);
-        pixDestroy(&pixb);
-        pixDestroy(&pix1);
-        pixDestroy(&pix2);
-    }
+	sarrayDestroy(&sargv);
 
-	snprintf(textstr, sizeof(textstr), "/tmp/lept/otsu/result.%s.pdf", sani_filename);
-	char* out_fullname = genPathname(textstr, NULL);
-	lept_stderr("Writing to: %s --> %s\n", textstr, out_fullname);
-    pixaConvertToPdf(pixad, 75, 1.0, 0, 0, "Otsu thresholding", out_fullname);
-	stringDestroy(&out_fullname);
-    bmfDestroy(&bmf);
-    pixDestroy(&pixs);
-    pixDestroy(&pixg);
-    pixaDestroy(&pixad);
-
-	LEPT_FREE(filename);
-	LEPT_FREE(sani_filename);
-	LEPT_FREE(filepath);
-  }
-
-    sarrayDestroy(&sargv);
-
-    return 0;
+	return 0;
 }
